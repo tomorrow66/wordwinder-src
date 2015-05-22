81d64408e65b2e1886d79fda74847ee5caf58e7c
DeAndre Johnson <dre@phreshr.com>
2015-05-22 15:09:46 -0400

    project updates

--- public/js/main.js
+++ public/js/main.js
@@ -63,6 +63,39 @@
       // }
     });
 
+    (function(){
+  
+      $.fn.fitHeights = function() {
+
+        var items = $(this);
+        function setHeights() {
+
+          var currentTallest = 0;
+
+          items.css({ 'min-height' : currentTallest });  // unset min-height to get actual new height
+
+          // right now this causes a noticeable shift in height on resize. workarounds?
+
+          items.each(function(){
+
+            if( $(this).height() > currentTallest ) { currentTallest = $(this).height(); }
+          });
+
+          items.css({ 'min-height' : currentTallest });
+
+        }
+
+        setHeights();
+        $(window).on('resize', setHeights);
+        return this;
+      };
+    })(jQuery);
+
+    $(window).load(function(){
+    	/* $(groupOfItems).fitHeights(); */
+      $('.grid-testimonials p').fitHeights();
+    });
+
   });
 
 }());
